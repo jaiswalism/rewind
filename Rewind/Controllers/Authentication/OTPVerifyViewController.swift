9f37efe39f0d56c256992670a3d56122664fc29a
ATG-AVI <imaviralshara@gmail.com>
2025-11-10 00:08:06 +0530

    feat: added back nav from going to otpverify to forget password

--- Rewind/Controllers/Authentication/OTPVerifyViewController.swift
+++ Rewind/Controllers/Authentication/OTPVerifyViewController.swift
@@ -21,16 +21,10 @@ class OTPVerifyViewController: UIViewController {
         let fields = [otpInput1, otpInput2, otpInput3, otpInput4]
         fields.forEach { $0?.styleRoundedInput() }
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
+    @IBAction func backButton(_ sender: Any) {
+        // Navigate back to ForgotPasswordViewController (XIB)
+              let forgotVC = ForgotPasswordViewController(nibName: "ForgotPasswordViewController", bundle: nil)
+              forgotVC.modalPresentationStyle = .fullScreen
+              present(forgotVC, animated: true, completion: nil)
+          }
+      }
