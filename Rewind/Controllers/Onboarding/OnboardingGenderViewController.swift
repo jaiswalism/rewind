75261403b60c46d828958f6c7666922cc5f4e614
Shyam Jaiswal <sj1464@srmist.edu.in>
2025-11-09 22:36:07 +0530

    added the select transition in the 2nd question, made other screens

--- Rewind/Controllers/Onboarding/OnboardingGenderViewController.swift
+++ Rewind/Controllers/Onboarding/OnboardingGenderViewController.swift
@@ -11,13 +11,97 @@ class OnboardingGenderViewController: UIViewController {
     
     @IBOutlet var genderButtons: [UIButton]!
     @IBOutlet weak var nextButton: UIButton!
+    
+    let maleSelectedImage = UIImage(named: "illustrations/onboarding/Male Selected")
+    let maleUnselectedImage = UIImage(named: "illustrations/onboarding/Male Unselected")
+    let femaleSelectedImage = UIImage(named: "illustrations/onboarding/Female Selected")
+    let femaleUnselectedImage = UIImage(named: "illustrations/onboarding/Female Unselected")
+
+    // Next Button Colors
+    let nextButtonEnabledColor = UIColor(named: "colors/Primary/Light")
+    let nextButtonDisabledColor = UIColor.systemGray4
+    
+    // Next Button Text Color
+    let nextButtonEnabledTextColor = UIColor(named: "colors/Blue&Shades/blue-400")
+    let nextButtonDisabledTextColor = UIColor.systemGray
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // --- Setup for Next Button ---
+        nextButton.configurationUpdateHandler = { [weak self] button in
+            guard let self = self else { return }
+            var config = button.configuration
+            if button.isEnabled {
+                config?.background.backgroundColor = self.nextButtonEnabledColor
+                config?.attributedTitle?.foregroundColor = self.nextButtonEnabledTextColor
+            } else {
+                config?.background.backgroundColor = self.nextButtonDisabledColor
+                config?.attributedTitle?.foregroundColor = self.nextButtonDisabledTextColor
+            }
+            button.configuration = config
+        }
+        
+        nextButton.isEnabled = false
+        
+        // --- Setup for Gender Buttons ---
+        for button in genderButtons {
+            button.configurationUpdateHandler = { [weak self] button in
+                self?.updateGenderButtonAppearance(for: button)
+            }
+            
+            button.layer.cornerRadius = 20
+            button.clipsToBounds = true
+        }
     }
 
+    @IBAction func genderButtonTapped(_ sender: UIButton) {
+        for button in genderButtons {
+            button.isSelected = (button == sender)
+        }
+        
+        nextButton.isEnabled = true
+    }
+
+    private func updateGenderButtonAppearance(for button: UIButton) {
+        
+        var config = button.configuration ?? .filled()
+        config.background.backgroundColor = UIColor(named: "colors/Blue&Shades/blue-300")
+        
+        if button.tag == 1 {
+            config.background.image = button.isSelected ? maleSelectedImage : maleUnselectedImage
+        } else if button.tag == 2 {
+            config.background.image = button.isSelected ? femaleSelectedImage : femaleUnselectedImage
+        }
+        
+        config.background.imageContentMode = .scaleToFill
+        
+        if button.isSelected {
+            applySelectedStyle(to: button)
+        } else {
+            applyUnselectedStyle(to: button)
+        }
+
+        button.configuration = config
+    }
+    
+    private func applySelectedStyle(to button: UIButton) {
+        button.layer.borderColor = UIColor.white.cgColor
+        button.layer.borderWidth = 2.0
+        
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.3
+        button.layer.shadowOffset = CGSize(width: 0, height: 4)
+        button.layer.shadowRadius = 5
+        
+        button.clipsToBounds = true
+        button.layer.masksToBounds = false
+    }
+    
+    private func applyUnselectedStyle(to button: UIButton) {
+        button.layer.borderWidth = 0
+        button.layer.shadowOpacity = 0
+    }
 
     /*
     // MARK: - Navigation
