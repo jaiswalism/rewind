46954a7ea82d396cca875867fffa3df4b51db0b7
Shyam Jaiswal <sj1464@srmist.edu.in>
2025-11-11 22:51:49 +0530

    feat: Added files for HomeTab, CareCorner, Journals, Setting & Notifications

---
+++ Rewind/Controllers/Journals/JournalsHomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  JournalsHomeViewController.swift
+//  Rewind
+//
+//  Created by Shyam on 11/11/25.
+//
+
+import UIKit
+
+class JournalsHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
